7a421b689dbf520e20fd43e3e0e61fb116792a18
Circuit <circuitsacul@icloud.com>
2022-05-21 14:53:12 -0400

    add favicon

--- circuitsacul/site.py
+++ circuitsacul/site.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import fastapi
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-
 app = fastapi.FastAPI()
+static = StaticFiles(directory="circuitsacul/website/static")
+app.mount("/static", static, name="static")
 templates = Jinja2Templates(directory="circuitsacul/website/pages")
 
 
